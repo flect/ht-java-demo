84858f7d16dac1947455062f03e1ffd77e1743fa
t.shimizu <shimizu@f.flect.co.jp>
2014-06-19 11:40:24 +0900

    Change url for hello world

--- src/main/java/jp/co/flect/heroku/training/HelloServlet.java
+++ src/main/java/jp/co/flect/heroku/training/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name="HelloServlet", urlPatterns={"/hello"})
+@WebServlet(name="HelloServlet", urlPatterns={"/"})
 public class HelloServlet extends HttpServlet {
 
 	@Override
