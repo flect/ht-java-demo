dba4395fa7dd09462aff0e9786d8ca1812508a01
Kei Ogasawara <kei.ogasawara@flect.co.jp>
2016-03-25 13:48:44 +0900

    Werckerのテストのための修正その4

--- src/main/java/jp/co/flect/heroku/training/HelloServlet.java
+++ src/main/java/jp/co/flect/heroku/training/HelloServlet.java
@@ -14,9 +14,6 @@ public class HelloServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		System.out.println("TEST_VARIABLE   : " + System.getenv("TEST_VARIABLE"));
-		res.getWriter().print("Good Moring World!");
-		res.getWriter().print("Hello World!");
-		res.getWriter().print("Good afternoon World!");
-		res.getWriter().print("Goodby World!");
+		res.getWriter().print("Hello World! Hello!");
 	}
 }
