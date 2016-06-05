f7c030f3c373fb68541e2464f40bdf0fd596620a
Kei Ogasawara <kei.ogasawara@flect.co.jp>
2016-06-06 08:28:05 +0900

    不要な画面表示を削除

--- src/main/java/jp/co/flect/heroku/training/HelloServlet.java
+++ src/main/java/jp/co/flect/heroku/training/HelloServlet.java
@@ -16,6 +16,5 @@ public class HelloServlet extends HttpServlet {
 		System.out.println("TEST_VARIABLE   : " + System.getenv("TEST_VARIABLE"));
 		System.out.println("Heroku App PORT   : " + System.getenv("PORT"));
 		res.getWriter().print("Hello World!");
-		res.getWriter().print("By review aap 2!");
 	}
 }
